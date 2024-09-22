81f8873af1fbde12ce9f76f711af937442d378b6
ahmetsagdasli <ahmetssagdasli@gmail.com>
2024-09-22 16:58:53 +0300

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,19 +1,23 @@
 import express from 'express'
 import mongoose from 'mongoose'
-import dotenv from "dotenv"
-dotenv.config();
+import dotenv from 'dotenv'
+import userRouter from "./routes/user.route.js";
 
-mongoose.connect(process.env.MONGO).then(
-  ()=>(console.log("MongoDB connection is successful."))
-).catch((err)=>(console.log("The connection has been failed.")))
+
+
+
+dotenv.config()
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log('MongoDB connection is successful.'))
+  .catch((err) => console.log('The connection has been failed.'))
 
 const app = express()
 const port = 3001
 
-app.get('/', (req, res) => {
-  res.send('merhaba')
-})
-
 app.listen(port, () => {
   console.log(`server ${port} portunda calisiyor`)
 })
+
+app.use("/api/user", userRouter)
